c6708c01307404540ffc7da41d56f4390f2fba34
VEERESH <veer.0963@gmail.com>
2020-02-26 22:09:36 +0530

    Factory pattern

---
+++ src/main/java/com/designpatterns/creational/factory/FactoryLaunch.java
@@ -0,0 +1,10 @@
+package com.designpatterns.creational.factory;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class FactoryLaunch {
+    public static void main(String[] args) {
+        log.info("General Kenobi");
+    }
+}
